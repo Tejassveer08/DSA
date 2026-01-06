a9a181da937114036121c141068ab3aaf83732a9
Tejassveer Vasant <147965837+Tejassveer08@users.noreply.github.com>
2026-01-06 23:38:42 +0530

    Time: 6 ms (35.30%) | Memory: 107 MB (96.77%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -11,26 +11,17 @@
  */
 class Solution {
 public:
+    vector<int> sum={INT_MIN};
+    void dfs(TreeNode* Node, int level=1){
+        if (Node==NULL) return;
+        if (sum.size()==level) sum.push_back(Node->val);
+        else sum[level]+=Node->val;
+        dfs(Node->left, level+1);
+        dfs(Node->right, level+1);
+    }
     int maxLevelSum(TreeNode* root) {
-        vector<long long> levelSum;
-        queue<TreeNode*> q;
-        long long sum = 0;
-        q.push(root);
-
-        while(!q.empty()) {
-            int size =  q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                sum += node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            levelSum.push_back(sum);
-            sum = 0;
-        }
-
-        return max_element(levelSum.begin(), levelSum.end()) - levelSum.begin() + 1;
+        dfs(root);
+        return max_element(sum.begin(), sum.end())-sum.begin();
+        
     }
 };
