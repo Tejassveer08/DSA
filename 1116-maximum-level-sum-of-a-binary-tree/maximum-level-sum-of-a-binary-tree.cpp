f1acec7727981030820bec4153502f3fadf5bae3
Tejassveer Vasant <147965837+Tejassveer08@users.noreply.github.com>
2026-01-06 23:37:24 +0530

    Time: 10 ms (14.55%) | Memory: 109.5 MB (59.06%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,34 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
+        vector<long long> levelSum;
         queue<TreeNode*> q;
+        long long sum = 0;
         q.push(root);
 
-        int level = 1;
-        int bestLevel = 1;
-        long long maxSum = LLONG_MIN;
-
-        while (!q.empty()) {
-            int sz = q.size();
-            long long currSum = 0;
-
-            for (int i = 0; i < sz; i++) {
+        while(!q.empty()) {
+            int size =  q.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                currSum += node->val;
+                sum += node->val;
 
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
-
-            if (currSum > maxSum) {
-                maxSum = currSum;
-                bestLevel = level;
-            }
-
-            level++;
+            levelSum.push_back(sum);
+            sum = 0;
         }
 
-        return bestLevel;
+        return max_element(levelSum.begin(), levelSum.end()) - levelSum.begin() + 1;
     }
 };
